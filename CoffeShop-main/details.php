8d1187557f8f75bc463b4ec03394b9f72c56a10a
ArefinReza <2211081064@uttarauniversity.edu.bd>
2024-10-19 23:40:05 +0600

    contact with me

--- CoffeShop-main/details.php
+++ CoffeShop-main/details.php
@@ -256,12 +256,12 @@ if ($result->num_rows > 0) {
 <section class="footer">
 
 <div class="share">
-    <a href="#" class="fab fa-facebook-f"></a>
-    <a href="#" class="fab fa-twitter"></a>
-    <a href="#" class="fab fa-instagram"></a>
-    <a href="#" class="fab fa-linkedin"></a>
-    <a href="#" class="fab fa-pinterest"></a>
-</div>
+        <a href="https://www.facebook.com/arefin.reza.saim4464444ud/" class="fab fa-facebook-f"></a>
+        <!-- <a href="#" class="fab fa-twitter"></a> -->
+        <a href="https://www.instagram.com/arefinsaim/" class="fab fa-instagram"></a>
+        <a href="https://www.linkedin.com/in/arefinreza46/" class="fab fa-linkedin"></a>
+        <!-- <a href="#" class="fab fa-pinterest"></a> -->
+    </div>
 
 <div class="links">
     <a href="index.php#home">Home</a>
